a6efe9de10bc3a9b0e652772bcc05367f7e579e1
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-29 16:44:51 +0530

    Using One Way Data Binding to display message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello from BridgeLabz</h1>
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      title: 'Hello From BridgeLabz'
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        {/*UC2: To display “Hello from BidgeLabz” using One Way Data Bindings */}
+      <h1> {this.state.title}</h1>
+      
      </div>
-  );
+    );
+  }
 }
-
 export default App;

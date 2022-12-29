4b2db050b5955427c3e9e756f4618ae61d1fa433
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-29 16:55:40 +0530

    Using Property Binding to display logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import logo from './logobl.png';
+
 class App extends React.Component {
   constructor(){
     super()
@@ -12,7 +14,9 @@ class App extends React.Component {
       <div className="App">
         {/*UC2: To display “Hello from BidgeLabz” using One Way Data Bindings */}
       <h1> {this.state.title}</h1>
-      
+      {/* UC3: To display BridgeLabz Logo using Property Binding Technique */}
+      <img src = {logo}
+      alt= "The BridgeLabz logo: a Bridge to employment through lab works"/>
      </div>
     );
   }

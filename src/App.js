9ef7c069b194e2f7061952c4ef2ffbda4872801c
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-29 16:23:43 +0530

    create hello react app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
+     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+     <img src={logo} className="App-logo" alt="logo" />
+      <p>
+  Edit <code>src/App.js</code> and save to reload.
+    </p>
+      <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
     </div>
   );
 }

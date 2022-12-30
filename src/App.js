81c934bba618f9130ee63406f1de8144fcb13db2
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-30 09:13:56 +0530

    Use Event Binding Technique  to bind click event

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import logo from './logobl.png';
 
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super()
     this.state = {
       title: 'Hello From BridgeLabz'
     }
+  }
+  // onClick function
+  onClick =($event)=> {
+    console.log("save button is clicked!!",$event);
+    window.open(this.url,"_blank");
   }
   render(){
     return (
@@ -15,8 +21,10 @@ class App extends React.Component {
         {/*UC2: To display “Hello from BidgeLabz” using One Way Data Bindings */}
       <h1> {this.state.title}</h1>
       {/* UC3: To display BridgeLabz Logo using Property Binding Technique */}
-      <img src = {logo}
+        {/* UC4: To launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo */}
+      <img src = {logo} onClick={this.onClick}
       alt= "The BridgeLabz logo: a Bridge to employment through lab works"/>
+    
      </div>
     );
   }

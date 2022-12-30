6e435053470231c2efb2efad3e145a4f1a0df59f
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-30 09:27:11 +0530

    To display Hello from BridgeLabz with a user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      title: 'Hello From BridgeLabz'
+      // specify userName as String Type in the App Component State
+      userName:''
     }
   }
   // onClick function
@@ -15,16 +16,26 @@ class App extends React.Component {
     console.log("save button is clicked!!",$event);
     window.open(this.url,"_blank");
   }
+  onNameChange=(event) => {
+    console.log("value is:",event.target.value);
+    //set the title using setState method
+    //bind the input from Element using setState to userName on on Change event
+    this.setState({userName: event.target.value})
+  }
+
   render(){
     return (
       <div className="App">
-        {/*UC2: To display “Hello from BidgeLabz” using One Way Data Bindings */}
-      <h1> {this.state.title}</h1>
+         {/* change in the userName reflected in the header Tag */}
+      <h1>Hello {this.state.userName} From BridgeLabz </h1>
       {/* UC3: To display BridgeLabz Logo using Property Binding Technique */}
         {/* UC4: To launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo */}
       <img src = {logo} onClick={this.onClick}
       alt= "The BridgeLabz logo: a Bridge to employment through lab works"/>
-    
+    {/*UC: 5 To display Hello from BridgeLabz with a user input name - Make sure to import Form Module */}
+   <div>
+    <input onChange={this.onNameChange} />
+   </div>
      </div>
     );
   }
